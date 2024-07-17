401496ae9a8b686953b05f1544976f24985d08bb
Olivier Cots <olivier.cots@irit.fr>
2024-07-17 10:28:53 +0200

    coverage ok

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Test
+using CalculusOfVariations
 
 #
 @testset verbose = true showtiming = true "Base" begin
--- test/test_default.jl
+++ test/test_default.jl
@@ -1,4 +1,4 @@
 # test_default
 function test_default()
-     @test true
+     @test CalculusOfVariations.greet() isa Nothing
 end
