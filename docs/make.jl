90643b2e141142a1a339f2a437e1cd9b3ffdc239
ocots <66357348+ocots@users.noreply.github.com>
2024-09-07 00:03:17 +0000

    :robot: Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,21 @@
 using Documenter
 
-makedocs(
-    remotes = nothing,
-    warnonly = :cross_references,
-    sitename = "Calculus of variations",
-    format = Documenter.HTML(
-        prettyurls = false,
-        size_threshold_ignore = ["index.md"],
-        assets = [
+makedocs(;
+    remotes=nothing,
+    warnonly=:cross_references,
+    sitename="Calculus of variations",
+    format=Documenter.HTML(;
+        prettyurls=false,
+        size_threshold_ignore=["index.md"],
+        assets=[
             asset("https://control-toolbox.org/assets/css/documentation.css"),
             asset("https://control-toolbox.org/assets/js/documentation.js"),
         ],
     ),
-    pages = [
+    pages=[
         "Introduction" => "index.md",
         "Catenoid solution" => "application-surface-revolution.md",
     ],
 )
 
-deploydocs(
-    repo = "github.com/control-toolbox/calculus_of_variations.git",
-    devbranch = "main",
-)
+deploydocs(; repo="github.com/control-toolbox/calculus_of_variations.git", devbranch="main")
